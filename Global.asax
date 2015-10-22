7f81f10a3ce89089f6cd616b9914fa40c9324870
miketti <acepost@protonmail.ch>
2015-10-22 16:23:17 +0300

    init

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication6_landing.MvcApplication" Language="C#" %>
---
+++ Helpers/JsonMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WebApplication6_landing.JsonResults
+{
+    public class JsonMessage
+    {
+        private bool _result;
+        private string _message;
+        private Object _object;
+
+        public bool Result
+        {
+            get { return _result; }
+            set { _result = value; }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+        public Object Object 
+        { 
+            get { return _object; } 
+            set { _object = value; } 
+        }
+
+    }
+}
