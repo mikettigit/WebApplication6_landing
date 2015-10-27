5c7924b38309ec1c7094d7596fd45e4bcbaf3703
miketti <acepost@protonmail.ch>
2015-10-27 10:22:48 +0300

    agava mail sending

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication6_landing.JsonResults;
@@ -25,37 +26,53 @@ namespace WebApplication6_landing.Controllers
             {
                 string receiver = collection["receiver"];
                 string message = collection["message"];
-
-                System.Web.Mail.MailMessage mail = new System.Web.Mail.MailMessage();
-
-                string SMTP_SERVER = "http://schemas.microsoft.com/cdo/configuration/smtpserver";
-                string SMTP_SERVER_PORT = "http://schemas.microsoft.com/cdo/configuration/smtpserverport";
-                string SEND_USING = "http://schemas.microsoft.com/cdo/configuration/sendusing";
-                string SMTP_USE_SSL = "http://schemas.microsoft.com/cdo/configuration/smtpusessl";
-                string SMTP_AUTHENTICATE = "http://schemas.microsoft.com/cdo/configuration/smtpauthenticate";
-                string SEND_USERNAME = "http://schemas.microsoft.com/cdo/configuration/sendusername";
-                string SEND_PASSWORD = "http://schemas.microsoft.com/cdo/configuration/sendpassword";
-
-                mail.Fields[SMTP_SERVER] = ConfigurationManager.AppSettings["SMTP"];
-                mail.Fields[SMTP_SERVER_PORT] = 465;
-                mail.Fields[SEND_USING] = 2;
-                mail.Fields[SMTP_USE_SSL] = true;
-                mail.Fields[SMTP_AUTHENTICATE] = 1;
-                mail.Fields[SEND_USERNAME] = ConfigurationManager.AppSettings["SMTP_login"];
-                mail.Fields[SEND_PASSWORD] = ConfigurationManager.AppSettings["SMTP_password"];
-
-                mail.From = ConfigurationManager.AppSettings["messageFrom"];
-                mail.To = ConfigurationManager.AppSettings["messageTo"];
-                mail.Subject = "Запрос";
-                mail.BodyFormat = System.Web.Mail.MailFormat.Text;
-                mail.Body = "Куда ответить: " + receiver + "\n";
+                string body = "Куда ответить: " + receiver + "\n";
+                string subject = "Запрос";
                 if (message != null)
                 {
-                    mail.Body += "Сообщение: " + message + "\n";
+                    body += "Сообщение: " + message + "\n";
                 }
 
-                System.Web.Mail.SmtpMail.SmtpServer = ConfigurationManager.AppSettings["SMTP"] + ":465";
-                System.Web.Mail.SmtpMail.Send(mail);
+                if (Convert.ToBoolean(ConfigurationManager.AppSettings["UseAgavaMail"]))
+                {
+                    MailMessage mailObj = new MailMessage();
+                    mailObj.From = new MailAddress(ConfigurationManager.AppSettings["messageFrom"]);
+                    mailObj.To.Add(ConfigurationManager.AppSettings["messageTo"]);
+                    mailObj.Subject = subject;
+                    mailObj.Body = body;
+
+                    SmtpClient SMTPServer = new SmtpClient("localhost");
+                    SMTPServer.Send(mailObj);
+                }
+                else
+                {
+                    System.Web.Mail.MailMessage mail = new System.Web.Mail.MailMessage();
+
+                    string SMTP_SERVER = "http://schemas.microsoft.com/cdo/configuration/smtpserver";
+                    string SMTP_SERVER_PORT = "http://schemas.microsoft.com/cdo/configuration/smtpserverport";
+                    string SEND_USING = "http://schemas.microsoft.com/cdo/configuration/sendusing";
+                    string SMTP_USE_SSL = "http://schemas.microsoft.com/cdo/configuration/smtpusessl";
+                    string SMTP_AUTHENTICATE = "http://schemas.microsoft.com/cdo/configuration/smtpauthenticate";
+                    string SEND_USERNAME = "http://schemas.microsoft.com/cdo/configuration/sendusername";
+                    string SEND_PASSWORD = "http://schemas.microsoft.com/cdo/configuration/sendpassword";
+
+                    mail.Fields[SMTP_SERVER] = ConfigurationManager.AppSettings["SMTP"];
+                    mail.Fields[SMTP_SERVER_PORT] = 465;
+                    mail.Fields[SEND_USING] = 2;
+                    mail.Fields[SMTP_USE_SSL] = true;
+                    mail.Fields[SMTP_AUTHENTICATE] = 1;
+                    mail.Fields[SEND_USERNAME] = ConfigurationManager.AppSettings["SMTP_login"];
+                    mail.Fields[SEND_PASSWORD] = ConfigurationManager.AppSettings["SMTP_password"];
+
+                    mail.From = ConfigurationManager.AppSettings["messageFrom"];
+                    mail.To = ConfigurationManager.AppSettings["messageTo"];
+                    mail.Subject = subject;
+                    mail.BodyFormat = System.Web.Mail.MailFormat.Text;
+                    mail.Body = body;
+
+                    System.Web.Mail.SmtpMail.SmtpServer = ConfigurationManager.AppSettings["SMTP"] + ":465";
+                    System.Web.Mail.SmtpMail.Send(mail);
+                }
 
                 jm.Result = true;
                 jm.Message = "Спасибо, запрос отправлен. Ожидайте ответ.";
