e340002baf68a1afe9c4578a6a35d90b2ca4460d
mikettigit <acepost@protonmail.ch>
2018-11-02 12:12:13 +0300

    mobile version fixes

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,7 +3,7 @@
     ready_projects = [
         {
             sample: "",
-            result: "http://aw-s.ru/",
+            result: "http://aw-s.ru/items/rozliv_%D0%9C-125",
             logo: "http://aw-s.ru/Content/img/logo.png",
             type: "Экспресс-сайт за 2 часа к выставке",
             name: "Автоматизированные водные системы"
@@ -42,8 +42,23 @@
             logo: "http://molbulal.ru/CONTENT/first.png",
             type: "Копия сайта",
             name: "Микрофинансы Мол Булал"
+        },
+        {
+            sample: "http://archive.is/ViIAX",
+            result: "//armystop.ru/",
+            logo: "http://armystop.ru/Content/logo.png",
+            type: "Копия сайта",
+            name: "Юр. компания ArmyStop"
+        },
+        {
+            sample: "http://archive.li/NPQfn",
+            result: "//энросса.рф/",
+            logo: "http://xn--80azdhga3f.xn--p1ai/logo_white.png",
+            type: "Копия сайта",
+            name: "Топливная компания Энросса"
         }
     ];
+    ready_projects = ready_projects.sort(function () { return 0.5 - Math.random() })
     
     $('#ready-projects').slick({
         infinite: true,
@@ -67,6 +82,7 @@
         ]
     });
 
+    var shown = 0;
     BaseItemsCount = $('#ready-projects .ready-project-item').length;
     for (var i = 0 ; i < ready_projects.length; i++) {
 
@@ -86,25 +102,27 @@
         var logo = instance.find(".ready-project-logo");
         logo.attr("src", ready_projects[i].logo)
         logo.load(function () {
-                $(this).off("load");
+            $(this).off("load");
+            shown++;
+            if (shown <= 3) {
                 $('#ready-projects').slick("slickAdd", $(this).parents(".ready-project-item"));
-            });
+                $('.ready-project-item .testblock').equivalent();
+            }
+        });
     }
 
-    $.fn.equivalent = function (){ 
+    $.fn.equivalent = function () {
         var $blocks = $(this);
         $blocks.css("height", "auto");
         maxH = $blocks.eq(0).outerHeight();
-        $blocks.each(function(){ 
+        $blocks.each(function () {
             maxH = ($(this).outerHeight() > maxH) ? $(this).outerHeight() : maxH;
-        }); 
+        });
         $blocks.outerHeight(maxH);
-    } 
-    $('.ready-project-item .testblock').equivalent();
+    }
     $(window).on("resize", function () {
         $('.ready-project-item .testblock').equivalent();
     });
-
 
     $(".re-assure2").click(function () {
         if (!$(this).hasClass("expand")) {
